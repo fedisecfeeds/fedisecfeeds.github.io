499ef0269bce109b606e1345f588442534a16c97
fedisecfeeds <fedisecfeeds@localhost>
2023-11-07 23:13:06 +1000

    Some UI Improvements.

--- renderer.py
+++ renderer.py
@@ -2,15 +2,16 @@
 
 from jinja2 import Environment, FileSystemLoader
 import json
-
+import datetime
 
 def render(jsonblob):
 	fedi_cve_feed = json.loads(open(jsonblob, 'r').read())
-
+	# sort
+	fedi_cve_feed = {k: v for k, v in sorted(fedi_cve_feed.items(), key=lambda item: item[1]['cvss3'], reverse=True)} 
 	# jinja2 rendering
 	environment = Environment()
 	template = environment.from_string(open("index.html.j2","r").read())
-	html = template.render(data=fedi_cve_feed)
+	html = template.render(data=fedi_cve_feed, updated=datetime.datetime.utcnow().isoformat())
 	with open('index.html', 'w+') as f:
 		f.write(html)
 	print('rendered to index.html')
