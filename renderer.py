6b503492eeb2a2da9912e85b2684ca1331bf1db6
fedisecfeeds <fedisecfeeds@localhost>
2023-11-14 18:23:05 +1000

    fixing epss

--- renderer.py
+++ renderer.py
@@ -4,10 +4,18 @@ from jinja2 import Environment, FileSystemLoader
 import json
 import datetime
 
+def get_key(kv_item):
+	d = item[1]
+	if 'epss' in d:
+		return d['epss']
+	else:
+		return 0
+
+
 def render(jsonblob):
 	fedi_cve_feed = json.loads(open(jsonblob, 'r').read())
 	# sort
-	fedi_cve_feed = {k: v for k, v in sorted(fedi_cve_feed.items(), key=lambda item:item[1]['epss'], reverse=True)} 
+	fedi_cve_feed = {k: v for k, v in sorted(fedi_cve_feed.items(), key=get_key, reverse=True)} 
 	# jinja2 rendering
 	environment = Environment()
 	template = environment.from_string(open("index.html.j2","r").read())
